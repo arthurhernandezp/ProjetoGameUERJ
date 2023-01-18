2a090b94658789027bda2063d9d690fa0e5860df
Arthur Hernandez <57412723+arthurhernandezp@users.noreply.github.com>
2023-01-17 23:52:50 -0300

    Update game.c

--- game.c
+++ game.c
@@ -1,5 +1,19 @@
 void rodaJogo(SDL_Renderer* ren,dadosPlayer *personagem,dadosCeu *ceu,dadosBarco *barco,dadosInventario *inventario,char * listaItens[],unsigned short int * screen){
-
+	//MINIGAME 
+	dadosMinigame minigame;
+	minigame.state = cancelado;
+    SDL_Rect rIsca = { 774,357, 45,60 };
+    SDL_Rect rPeixe = { 655,425, 26,19 };
+	SDL_Texture* isca = IMG_LoadTexture(ren, "imgs/minigameIsca.png");
+	SDL_Texture* peixeMG = IMG_LoadTexture(ren, "imgs/minigamePeixe.png");
+	SDL_Texture* miniGameBG = IMG_LoadTexture(ren, "imgs/minigamepesca2.png");
+	SDL_Rect miniGame = { 620,380,308,66};
+	unsigned short int iscaSpeed = 5;
+	unsigned short int peixeSpeed = 0;
+	unsigned short int cur_state = 0;
+	
+	
+	//FIM DO MINIGAME
 	int i = 0; int contFundo = 0;
 	dadosMouse mouse;
 	mouse.rect.x = 0; mouse.rect.y = 0; mouse.rect.w = 30; mouse.rect.h = 50;
@@ -41,7 +55,7 @@ void rodaJogo(SDL_Renderer* ren,dadosPlayer *personagem,dadosCeu *ceu,dadosBarco
 	Uint32 antes = 0;
 	int var = 1;
 	int randAux = 0;
-	
+	unsigned short int pescaAux = 0;
 	while (*screen == jogo) {	
 		espera = MAX(espera - (int)(SDL_GetTicks() - antes), 0);
 	  	SDL_Event evt; int isevt = AUX_WaitEventTimeoutCount(&evt,&espera);    
@@ -138,56 +152,13 @@ void rodaJogo(SDL_Renderer* ren,dadosPlayer *personagem,dadosCeu *ceu,dadosBarco
 							if(inventario->state == fechado) inventario->state = aberto;
 							else if(inventario->state == aberto) inventario->state = fechado;
 							break;
-						case SDLK_1:
-							if(inventario->state == aberto){
-								randAux = rand() % 7;	
-								if(!listaCheia(*inventario)){
-									if(	!buscaElemento(*inventario,inventario->i,inventario->j) ){
-										printf("[%d,%d]INSERINDO\n",inventario->i,inventario->j);
-										inventario->matrizItens[inventario->i][inventario->j].img = IMG_LoadTexture(ren, listaItens[randAux]);
-										inventario->matrizItens[inventario->i][inventario->j].state = true;
-										(inventario->n)++;
-										inventario->j++;
-
-										if(inventario->j >= 2 && inventario->i<5){
-										inventario->j = 0;
-										inventario->i++;
-										}
-									}
-									else{
-										printf("Existe um elemento na proxima posição!!!");
-									}
-								}
-								else{
-									printf("Lista Cheia!!!!\n");
-								}
-							}
-							break;
-						case SDLK_2:
-							if(inventario->state == aberto){
-								if(!listaVazia(*inventario)){
-									inventario->j--;
-									if(inventario->j < 0 && inventario->i>0){
-										inventario->j = 1;
-										inventario->i--;
-									}
-									inventario->matrizItens[inventario->i][inventario->j].img = NULL;
-									inventario->matrizItens[inventario->i][inventario->j].state = false;
-									(inventario->n)--;
-									printf("[%d,%d] DELETING\n",inventario->i,inventario->j);
-								}
-								else{
-									printf("Lista Vazia!!!!\n");
-								}
-							}
-							break;
 						default:
 							SDL_FlushEvent(evt.type);
 							break;
 		 		}
 		 		case SDL_MOUSEMOTION:
 		 			SDL_GetMouseState(&mouse.point.x,&mouse.point.y);
-		 			if(personagem->lugar == onBoat && SDL_PointInRect(&mouse.point,&w) && *screen == jogo){
+		 			if(personagem->lugar == onBoat && SDL_PointInRect(&mouse.point,&w) && *screen == jogo || personagem->state == fishing || personagem->state == pulling){
 		 				mouse.state = 1;
 		 				mouse.rect.x = mouse.point.x - mouse.rect.w;
 		 				mouse.rect.y = mouse.point.y- mouse.rect.h;
@@ -196,27 +167,140 @@ void rodaJogo(SDL_Renderer* ren,dadosPlayer *personagem,dadosCeu *ceu,dadosBarco
 		 		default:
 					SDL_FlushEvent(evt.type);
 					break;
+				case SDL_MOUSEBUTTONDOWN:
+					if(mouse.state == 1 && personagem->state != fishing && personagem->state != pulling){
+						personagem->state = fishing;
+						personagem->rect.y -= 5;
+					}
+					if(evt.button.state==SDL_PRESSED) {
+					 	cur_state = clicking;
+					}
+                    if(personagem->state == pulling) iscaSpeed = rand()%5+10;
+                break;
+                case SDL_MOUSEBUTTONUP:
+				   if(cur_state == clicking) cur_state = clicked;
+				   if(personagem->state == pulling) iscaSpeed = rand()%5+12;
+				   break;
 			}
 		} else {   
-  			contFundo += 1;
- 			if (contFundo == 15){
-				(ceu->fundoAux)++;
-				contFundo = 0;
+			if(personagem->state != pulling){
+	  			contFundo += 1;
+	 			if (contFundo == 15){
+					(ceu->fundoAux)++;
+					contFundo = 0;
+				}
+				w.y = w.y + 1 * (var);
+				var *= -1;
+				espera = 500;
 			}
-			w.y = w.y + 1 * (var);
-			var *= -1;
 			if(personagem->lugar == onGround){
 				personagem->state = idle;
 				if (personagem->corte.x < 240) personagem->corte.x +=48;
 				else personagem->corte.x = 0;
 			}
-			espera = 500;
+			else if(personagem->lugar == onBoat){
+				if(personagem->state == fishing){
+					if(personagem->corte.x < 144) personagem->corte.x +=48;
+					else personagem->corte.x = 0;
+					pescaAux++;
+					if(pescaAux == 10) {
+						personagem->state = pulling;
+						pescaAux = 0;
+						personagem->corte.x = 0;
+						randAux = rand() % 7;
+						switch(randAux){
+							case 0:
+								peixeSpeed = 1;
+							break;
+							case 1:
+								peixeSpeed = 2;
+							break;
+							case 2: 
+								peixeSpeed = 3;
+							break;
+							default:
+								peixeSpeed = 4;
+							break;
+						}
+					}
+				}
+				else if(personagem->state == pulling){
+					minigame.state = emjogo;
+					if(pescaAux == 5){
+						if(personagem->corte.x < 96) personagem->corte.x += 48;
+						else personagem->corte.x = 0;
+						pescaAux = 0;
+					}
+					if(cur_state == clicking){
+        				rIsca.x += iscaSpeed;
+						if(rIsca.x < rPeixe.x) rPeixe.x += peixeSpeed;
+						else rPeixe.x += 1;
+        			}
+					if(cur_state == clicking){
+						rIsca.x += iscaSpeed;
+						if(rIsca.x < rPeixe.x) rPeixe.x += peixeSpeed;
+						else rPeixe.x += 1;
+					}
+					else rIsca.x -= iscaSpeed;
+					espera = 100;
+					pescaAux++;
+				}
+			}
 		}
+		
+		if(*screen == menu) {
+			mouse.state = 0;
+			SDL_ShowCursor(true);
+		}
+		//trata minigame
+		if(personagem->state != pulling && minigame.state == emjogo){
+			minigame.state = cancelado;
+			personagem->state = remando;
+		}
+		else{
+			if(rPeixe.x >= 875){
+				minigame.state == cancelado;
+				if(!listaCheia(*inventario)){
+					printf("[%d,%d]INSERINDO\n",inventario->i,inventario->j);
+					inventario->matrizItens[inventario->i][inventario->j].img =IMG_LoadTexture(ren, listaItens[randAux]); 
+					inventario->matrizItens[inventario->i][inventario->j].state = true;
+					(inventario->n)++;
+					inventario->j++;
+					if(inventario->j >= 2 && inventario->i<5){
+						inventario->j = 0;
+						inventario->i++;
+					}
+				}
+				else{
+					printf("Lista Cheia!!!!\n");
+				}
+				rPeixe.x = 655; 
+				rIsca.x = 774;
+				personagem->state = remando;
+			}
+			if(rIsca.x <= 620 || rIsca.x >= (928-rIsca.w)) {
+				rIsca.x = 774;
+				rPeixe.x = 655;
+				minigame.state = cancelado;
+				personagem->state = remando;
+			}
+		}
+		
 		//MUDA TEXTURA DO PLAYER
 		if(personagem->state == idle && personagem->lugar == onGround) personagem->texture = IMG_LoadTexture(ren, "imgs/Fisherman_idle.png");
 		else if(personagem->state == walking && personagem->lugar == onGround ) personagem->texture = IMG_LoadTexture(ren, "imgs/fisherman2.png");
-		else if(personagem->lugar == onBoat ) personagem->texture = IMG_LoadTexture(ren, "imgs/Fisherman_row.png");
-		
+		else if(personagem->lugar == onBoat && personagem->state == remando) {
+			personagem->texture = IMG_LoadTexture(ren, "imgs/Fisherman_row.png");
+			personagem->rect.y = 473;
+		}
+		else if(personagem->lugar == onBoat && personagem->state == fishing){
+			personagem->texture = IMG_LoadTexture(ren, "imgs/Fisherman_fish.png");
+			personagem->rect.y = 467;
+			personagem->corte.y = 0;
+		}
+		else if(personagem->lugar == onBoat && personagem->state == pulling){
+			personagem->texture = IMG_LoadTexture(ren, "imgs/Fisherman_hook.png");
+		}
 		switch(ceu->fundoAux){
 			case 1:
 				ceu->texture = IMG_LoadTexture(ren, "imgs/bg3.png");
@@ -247,7 +331,6 @@ void rodaJogo(SDL_Renderer* ren,dadosPlayer *personagem,dadosCeu *ceu,dadosBarco
 			break;
 		}
 		
-		
 		SDL_RenderClear(ren);
 		
 		SDL_RenderCopy(ren, ceu->texture, NULL, NULL);		
@@ -257,8 +340,18 @@ void rodaJogo(SDL_Renderer* ren,dadosPlayer *personagem,dadosCeu *ceu,dadosBarco
 			SDL_RenderCopy(ren, listaDec[i], NULL, &gDec);
 			gDec.x+=100;
 		} gDec.x = 0;	
-		SDL_RenderCopy(ren, barco->texture, NULL, &barco->rect);		
-		SDL_RenderCopy(ren, personagem->texture, &personagem->corte, &personagem->rect);
+		
+		if(personagem->state != fishing && personagem->state != pulling){
+			SDL_RenderCopy(ren, barco->texture, NULL, &barco->rect);		
+			SDL_RenderCopy(ren, personagem->texture, &personagem->corte, &personagem->rect);
+		}
+		else{
+			SDL_RenderCopy(ren, personagem->texture, &personagem->corte, &personagem->rect);
+			SDL_RenderCopy(ren, barco->texture, NULL, &barco->rect);		
+		}
+		
+		
+		
 		SDL_RenderCopy(ren, agua, NULL, &w);
 		SDL_RenderCopy(ren, grama, NULL, &g);	
 		
@@ -270,12 +363,16 @@ void rodaJogo(SDL_Renderer* ren,dadosPlayer *personagem,dadosCeu *ceu,dadosBarco
 		else if(mouse.state == 0) SDL_ShowCursor(true);
 		
 		if( (personagem->rect.x >= barco->rect.x && personagem->lugar == onGround) ||
-		(personagem->rect.x > 554 && personagem->rect.x < 652 && personagem->lugar == onBoat ) ) {
+		(personagem->rect.x > 554 && personagem->rect.x < 652 && personagem->lugar == onBoat ) && personagem->state !=pulling && personagem->state != fishing) {
 			SDL_RenderCopy(ren, botao.texture, NULL, &botao.rect);
 		}	
 		
 		if(inventario->state == aberto) chamaInventario(ren,*inventario);
-		
+		if(minigame.state == emjogo && personagem->state == pulling){
+			SDL_RenderCopy(ren, miniGameBG, NULL, &miniGame);
+			SDL_RenderCopy(ren, peixeMG, NULL, &rPeixe);
+			SDL_RenderCopy(ren, isca, NULL, &rIsca);
+		}
 		SDL_RenderPresent(ren);
 		
 	}	
