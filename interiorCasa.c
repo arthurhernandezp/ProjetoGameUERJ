326919c1bc2bad6be08012ad67419a1d9ce210e5
arthurhernandezp <arthurhernandezp@gmail.com>
2024-06-19 10:30:07 -0300

    Funcao de controle do interior da casa

---
+++ interiorCasa.c
@@ -0,0 +1,66 @@
+void interiorCasa(SDL_Renderer* ren,dadosPlayer *personagem,dadosBarco *barco,dadosCeu *ceu,uint8_t * screen){
+	if(*screen == casa){
+		TTF_Init();
+		SDL_Color padrao = { 255,0,0,255 };
+		SDL_Color focus = { 0,157,231,255 };
+		char fonte[23] = "imgs/ArcadeClassic.ttf";
+		TTF_Font *ourFont = TTF_OpenFont(fonte,40);
+		Uint32 antes = 0;
+		int espera = 0;    
+		SDL_Rect aguaRect1 = {0,540,584,64};
+		SDL_Rect aguaRect2 = {454,540,584,64};
+		barco->rect = (SDL_Rect) { -100,513, 148,36 };
+		personagem->rect.y = 473;
+		personagem->rect.x = (barco->rect.w)/2 + barco->rect.x;
+		personagem->texture = IMG_LoadTexture(ren, "imgs/Fisherman_row.png");
+		SDL_Texture* agua = IMG_LoadTexture(ren, "imgs/Water.png");
+		personagem->corte.y = personagem->corte.x = 0;
+		
+		SDL_Texture* fundoCasa = IMG_LoadTexture(ren, "imgs/interior_house.jpg");
+		
+		SDL_Surface* textoSur = TTF_RenderText_Solid(ourFont, "Aperte    ESC     para    sair",padrao);
+		SDL_Texture* textoTex = SDL_CreateTextureFromSurface(ren,textoSur);
+		SDL_Rect textoRect = {300,-100,600,200};
+		SDL_FreeSurface(textoSur);
+		int var = 1;
+		short int aguaAux = 0;
+		while(*screen == casa){
+			espera = MAX(espera - (int)(SDL_GetTicks() - antes), 0);
+		  	SDL_Event evento; int isevt = AUX_WaitEventTimeoutCount(&evento,&espera);    
+		  	antes = SDL_GetTicks();	
+		  	if(isevt){ 
+		  		switch(evento.type){
+		  			case SDL_KEYUP:
+		  				if(evento.key.keysym.sym == SDLK_ESCAPE) *screen = fim;
+		  				break;
+		  			case SDL_QUIT:
+		  				*screen = fim;
+		  				break;
+		  			default:
+		  				SDL_FlushEvent(evento.type);
+		  		}
+		  	}else{
+		  		espera = 100;
+		  		if(barco->rect.x <= 1200){
+		  			barco->rect.x +=2;
+		  			personagem->rect.x += 2;
+		  			if(personagem->corte.x < 144) personagem->corte.x +=48;
+					else personagem->corte.x = 0;
+		  		}
+		  		if(textoRect.y <= 150) textoRect.y+= 2;
+			}
+			SDL_RenderClear(ren);
+			SDL_RenderCopy(ren, fundoCasa, NULL, NULL);				
+			SDL_RenderCopy(ren, barco->texture, NULL, &barco->rect);
+			SDL_RenderCopy(ren, agua, NULL, &aguaRect1);	
+			SDL_RenderCopy(ren, agua, NULL, &aguaRect2);		
+			SDL_RenderCopy(ren, personagem->texture, &personagem->corte, &personagem->rect);	
+			SDL_RenderCopy(ren,textoTex ,NULL,&textoRect);
+			SDL_RenderPresent(ren);		
+		}
+	}
+}
+	
+
+
+
