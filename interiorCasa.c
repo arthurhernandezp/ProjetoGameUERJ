f2e7a28b11d8b24b2526ada40ab32dc8d16bbb7d
arthurhernandezp <arthurhernandezp@gmail.com>
2024-06-19 15:44:35 -0300

    Mudança no tamanho do fundo da casa

--- interiorCasa.c
+++ interiorCasa.c
@@ -3,7 +3,7 @@ void interiorCasa(SDL_Renderer* ren,dadosPlayer *personagem,dadosInventario *inv
 		Uint32 antes = 0;
 		int espera = 0;    
 		
-		SDL_Rect casaRect = {74,63,852,474};
+		SDL_Rect casaRect = {124,103,752,394};
 		SDL_Texture* fundoCasa = IMG_LoadTexture(ren, "imgs/interior_house.jpg");
 		
 		dadosBotao botao;
@@ -19,7 +19,7 @@ void interiorCasa(SDL_Renderer* ren,dadosPlayer *personagem,dadosInventario *inv
 		  	if(isevt){ 
 		  		switch(evento.type){
 		  			case SDL_KEYUP:
-		  				if(evento.key.keysym.sym == SDLK_e && personagem->rect.x >= 758){
+		  				if(evento.key.keysym.sym == SDLK_e && personagem->rect.x >= 709){
 		  					 *screen = jogo;
 		  					 personagem->rect.y= 415;
 		  					 personagem->rect.x=368;
@@ -48,7 +48,7 @@ void interiorCasa(SDL_Renderer* ren,dadosPlayer *personagem,dadosInventario *inv
 		  				break;
 		  			case SDL_KEYDOWN:
 		  				if(evento.key.keysym.sym == SDLK_RIGHT){
-							if(personagem->rect.x < 826){
+							if(personagem->rect.x < 779){
 								if(personagem->corte.x < 240){
 								 	personagem->corte.x += 48;
 								}
@@ -63,7 +63,7 @@ void interiorCasa(SDL_Renderer* ren,dadosPlayer *personagem,dadosInventario *inv
 							}
 								
 		  				}else if(evento.key.keysym.sym == SDLK_LEFT){	
-							if(personagem->rect.x > 105){
+							if(personagem->rect.x > 170){
 									if(personagem->corte.x < 240) personagem->corte.x +=48;
 									else personagem->corte.x = 0;
 									if(personagem->corte.y != 48) personagem->corte.y = 48;
@@ -94,8 +94,8 @@ void interiorCasa(SDL_Renderer* ren,dadosPlayer *personagem,dadosInventario *inv
 			
 			SDL_RenderCopy(ren, fundoCasa, NULL, &casaRect);					
 			SDL_RenderCopy(ren, personagem->texture, &personagem->corte, &personagem->rect);	
-			if(personagem->rect.x >= 758){
-				botao.rect.x = 800;
+			if(personagem->rect.x >= 709){
+				botao.rect.x = 761;
 				botao.rect.y = 250;
 				SDL_RenderCopy(ren, botao.texture, NULL, &botao.rect);
 			}
