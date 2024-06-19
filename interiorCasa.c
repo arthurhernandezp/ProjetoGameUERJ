49c56d37f1bb5ade9d8489aca990b26ecad97f5a
arthurhernandezp <arthurhernandezp@gmail.com>
2024-06-19 11:43:00 -0300

    Adaptação do código para o interior da casa, antes estava a copia de tela final

--- interiorCasa.c
+++ interiorCasa.c
@@ -1,29 +1,21 @@
-void interiorCasa(SDL_Renderer* ren,dadosPlayer *personagem,dadosBarco *barco,dadosCeu *ceu,uint8_t * screen){
+void interiorCasa(SDL_Renderer* ren,dadosPlayer *personagem,uint8_t * screen){
 	if(*screen == casa){
-		TTF_Init();
-		SDL_Color padrao = { 255,0,0,255 };
-		SDL_Color focus = { 0,157,231,255 };
-		char fonte[23] = "imgs/ArcadeClassic.ttf";
-		TTF_Font *ourFont = TTF_OpenFont(fonte,40);
 		Uint32 antes = 0;
 		int espera = 0;    
-		SDL_Rect aguaRect1 = {0,540,584,64};
-		SDL_Rect aguaRect2 = {454,540,584,64};
-		barco->rect = (SDL_Rect) { -100,513, 148,36 };
-		personagem->rect.y = 473;
-		personagem->rect.x = (barco->rect.w)/2 + barco->rect.x;
-		personagem->texture = IMG_LoadTexture(ren, "imgs/Fisherman_row.png");
-		SDL_Texture* agua = IMG_LoadTexture(ren, "imgs/Water.png");
+
+		personagem->rect.y = 320;
+		personagem->rect.x = 800;
 		personagem->corte.y = personagem->corte.x = 0;
 		
+		SDL_Rect casaRect = {74,63,852,474};
 		SDL_Texture* fundoCasa = IMG_LoadTexture(ren, "imgs/interior_house.jpg");
 		
-		SDL_Surface* textoSur = TTF_RenderText_Solid(ourFont, "Aperte    ESC     para    sair",padrao);
-		SDL_Texture* textoTex = SDL_CreateTextureFromSurface(ren,textoSur);
-		SDL_Rect textoRect = {300,-100,600,200};
-		SDL_FreeSurface(textoSur);
-		int var = 1;
-		short int aguaAux = 0;
+		dadosBotao botao;
+		botao.rect = (SDL_Rect) {632,375,35,35};
+		botao.texture = IMG_LoadTexture(ren, "imgs/botao.png");
+		
+		SDL_SetRenderDrawColor(ren,0,0,0,255);
+		
 		while(*screen == casa){
 			espera = MAX(espera - (int)(SDL_GetTicks() - antes), 0);
 		  	SDL_Event evento; int isevt = AUX_WaitEventTimeoutCount(&evento,&espera);    
@@ -31,31 +23,68 @@ void interiorCasa(SDL_Renderer* ren,dadosPlayer *personagem,dadosBarco *barco,da
 		  	if(isevt){ 
 		  		switch(evento.type){
 		  			case SDL_KEYUP:
-		  				if(evento.key.keysym.sym == SDLK_ESCAPE) *screen = fim;
+		  				if(evento.key.keysym.sym == SDLK_e && personagem->rect.x >= 758){
+		  					 *screen = jogo;
+		  					 personagem->rect.y= 415;
+		  					 personagem->rect.x=368;
+		  					 break;
+		  				}
+		  				if(evento.key.keysym.sym == SDLK_RIGHT || evento.key.keysym.sym == SDLK_LEFT){
+			 				 personagem->state = idle;
+			 			}
+						#ifdef DEBUG
+							printf("\nTecla solta: %s\n", nomeTecla(evento.key.keysym.sym));
+    						printf("Estado do personagem: %d\n", personagem->state);
+                            printf("Posicao x: %d | Posicao y: %d\n", personagem->rect.x,personagem->rect.y);
+						#endif
 		  				break;
 		  			case SDL_QUIT:
 		  				*screen = fim;
+		  				break;
+		  			case SDL_KEYDOWN:
+		  				if(evento.key.keysym.sym == SDLK_RIGHT){
+							if(personagem->rect.x < 826){
+								if(personagem->corte.x < 240){
+								 	personagem->corte.x += 48;
+								}
+								else {
+									personagem->corte.x = 0;
+								}
+								
+								if(personagem->corte.y != 0) personagem->corte.y = 0;
+								
+								personagem->rect.x += 7;
+								personagem->state = walking;
+							}
+								
+		  				}else if(evento.key.keysym.sym == SDLK_LEFT){	
+							if(personagem->rect.x > 105){
+									if(personagem->corte.x < 240) personagem->corte.x +=48;
+									else personagem->corte.x = 0;
+									if(personagem->corte.y != 48) personagem->corte.y = 48;
+									
+									personagem->rect.x -= 7;
+									personagem->state = walking;
+							}
+						}
 		  				break;
 		  			default:
 		  				SDL_FlushEvent(evento.type);
 		  		}
 		  	}else{
-		  		espera = 100;
-		  		if(barco->rect.x <= 1200){
-		  			barco->rect.x +=2;
-		  			personagem->rect.x += 2;
-		  			if(personagem->corte.x < 144) personagem->corte.x +=48;
-					else personagem->corte.x = 0;
-		  		}
-		  		if(textoRect.y <= 150) textoRect.y+= 2;
+		  		espera = 500;
 			}
+			mudaTextura(ren,personagem);
 			SDL_RenderClear(ren);
-			SDL_RenderCopy(ren, fundoCasa, NULL, NULL);				
-			SDL_RenderCopy(ren, barco->texture, NULL, &barco->rect);
-			SDL_RenderCopy(ren, agua, NULL, &aguaRect1);	
-			SDL_RenderCopy(ren, agua, NULL, &aguaRect2);		
+			//Desenha o botao "E"
+			
+			SDL_RenderCopy(ren, fundoCasa, NULL, &casaRect);					
 			SDL_RenderCopy(ren, personagem->texture, &personagem->corte, &personagem->rect);	
-			SDL_RenderCopy(ren,textoTex ,NULL,&textoRect);
+			if(personagem->rect.x >= 758){
+				botao.rect.x = 800;
+				botao.rect.y = 250;
+				SDL_RenderCopy(ren, botao.texture, NULL, &botao.rect);
+			}
 			SDL_RenderPresent(ren);		
 		}
 	}
