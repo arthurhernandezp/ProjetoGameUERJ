7ce495667c5e165ac97e20a35bb91d52c8463f5b
Arthur Hernandez <57412723+arthurhernandezp@users.noreply.github.com>
2024-07-10 11:13:20 -0300

    Update controleJoystick.ino

--- controleJoystick/controleJoystick.ino
+++ controleJoystick/controleJoystick.ino
@@ -1,40 +1,47 @@
-const int xPin = A0; // Pino ADC para o eixo X
-const int yPin = A1; // Pino ADC para o eixo Y
-const int buttonPin = 2; // Pino digital para o botão
+const int xPin = A0;
+const int yPin = A1;
+const int buttonPin = 2;
+
+const int PIN_botaoEsq = 8;
+const int PIN_botaoMeio = 9;
+const int PIN_botaoDir = 10;
+
+int botaoEsq,botaoMeio,botaoDir = 0;
+int xDirection = 0;
+int yDirection = 0;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(buttonPin, INPUT_PULLUP); // Configurando o pino do botão como entrada com pull-up interno
+    pinMode(buttonPin, INPUT_PULLUP); 
+    pinMode(PIN_botaoEsq,INPUT_PULLUP);
+    pinMode(PIN_botaoMeio,INPUT_PULLUP);
+    pinMode(PIN_botaoDir,INPUT_PULLUP);
 }
 
 void loop() {
-int xValue = analogRead(A0);  // Supondo que o eixo X está conectado ao pino A0
-  int yValue = analogRead(A1);  // Supondo que o eixo Y está conectado ao pino A1
-  int buttonState = digitalRead(2); // Supondo que o botão está conectado ao pino 2
+  int xValue = analogRead(A0);  
+  int yValue = analogRead(A1); 
+  int buttonState = (!digitalRead(2));
 
-  int xDirection = 0;
-  int yDirection = 0;
-
-  // Determina a direção do eixo X
-  if (xValue < 400) {
-    xDirection = -1;
-  } else if (xValue > 600) {
-    xDirection = 1;
-  }
-
-  // Determina a direção do eixo Y
-  if (yValue < 400) {
-    yDirection = -1;
-  } else if (yValue > 600) {
-    yDirection = 1;
-  }
-
-  // Envia os dados na formatação: xDirection,yDirection,buttonState
+  xDirection = map(xValue,0,1023,-4,5);
+  yDirection = map(yValue,0,1023,-4,5);
+  
+  botaoEsq = (!digitalRead(PIN_botaoEsq));
+  botaoMeio = (!digitalRead(PIN_botaoMeio));
+  botaoDir = (!digitalRead(PIN_botaoDir));
+  
+  // Envia os dados na formatação: xDirection,yDirection,buttonState,botaoEsq,botaoMeio,botaoDir
   Serial.print(xDirection);
   Serial.print(",");
   Serial.print(yDirection);
   Serial.print(",");
-  Serial.println(buttonState);
-
+  Serial.print(buttonState);
+  Serial.print(",");
+  Serial.print(botaoEsq);
+  Serial.print(",");
+  Serial.print(botaoMeio);
+  Serial.print(",");
+  Serial.println(botaoDir);
+  
   delay(100);  // Intervalo de 100 ms entre as leituras
 }
